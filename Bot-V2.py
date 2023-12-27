c259af8f96be63fa9db0c2fe899d7c28ee13fe72
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-12-27 15:27:31 +0300

    V2.0

--- Bot-V2.py
+++ Bot-V2.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
-from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
@@ -15,6 +14,22 @@ dp = Dispatcher(bot, storage=storage)
 STATE_CAR_BRAND = 'state_car_brand'
 STATE_CAR_MODEL = 'state_car_model'
 STATE_CAR_YEAR = 'state_car_year'
+STATE_CAR_BODY_TYPE = 'state_car_body_type'
+STATE_CAR_ENGINE_TYPE = 'state_car_engine_type'
+STATE_CAR_ENGINE_VOLUME = 'state_car_engine_volume'
+STATE_CAR_POWER = 'state_car_power'
+STATE_CAR_TRANSMISSION_TYPE = 'state_car_transmission_type'
+STATE_CAR_COLOR = 'state_car_color'
+STATE_CAR_MILEAGE = 'state_car_mileage'
+STATE_CAR_DOCUMENT_STATUS = 'state_car_document_status'
+STATE_CAR_OWNERS = 'state_car_owners'
+STATE_CAR_CUSTOMS_CLEARED = 'state_car_customs_cleared'
+STATE_CAR_PHOTO = 'state_car_photo'
+STATE_CAR_DESCRIPTION = 'state_car_description'
+STATE_CAR_PRICE = 'state_car_price'
+STATE_CAR_LOCATION = 'state_car_location'
+STATE_SELLER_NAME = 'state_seller_name'
+STATE_SELLER_PHONE = 'state_seller_phone'
 
 
 # Обработка команды /start
@@ -22,99 +37,45 @@ STATE_CAR_YEAR = 'state_car_year'
 async def cmd_start(event: types.Message, state: FSMContext):
     user_id = event.from_user.id
     await event.answer(f"Привет, {event.from_user.first_name}! Я бот для сбора данных. Давай начнем.")
-
-    # Инициализируем user_data в FSMContext
     await state.update_data(user_data={"user_id": user_id})
-
-    # Задаем первый вопрос
-    await event.answer("Напишите бренд автомобиля.:")
-
-    # Устанавливаем состояние пользователя в STATE_CAR_BRAND
+    await event.answer("Напишите бренд автомобиля:")
     await state.set_state(STATE_CAR_BRAND)
 
 
 @dp.message_handler(state=STATE_CAR_BRAND)
 async def get_car_brand(event: types.Message, state: FSMContext):
-    user_id = event.from_user.id
-    car_brand = event.text
-
-    # Получаем текущие данные из FSMContext
-    data = await state.get_data()
-    user_data = data.get("user_data", {})
-
-    # Добавляем новые данные
-    user_data["car_brand"] = car_brand
-
-    # Обновляем данные в FSMContext
+    user_data = (await state.get_data()).get("user_data", {})
+    user_data["car_brand"] = event.text
     await state.update_data(user_data=user_data)
-
-    # Дополнительная логика обработки ответа, если необходимо
-    # Например, задать следующий вопрос или выполнить другие действия
-
-    # Задаем второй вопрос
     await event.answer("Хорошо! Укажите модель автомобиля:")
-
-    # Устанавливаем состояние пользователя в STATE_CAR_MODEL
     await state.set_state(STATE_CAR_MODEL)
 
 
 @dp.message_handler(state=STATE_CAR_MODEL)
 async def get_car_model(event: types.Message, state: FSMContext):
-    user_id = event.from_user.id
-    car_model = event.text
-
-    # Получаем текущие данные из FSMContext
-    data = await state.get_data()
-    user_data = data.get("user_data", {})
-
-    # Добавляем новые данные
-    user_data["car_model"] = car_model
-
-    # Обновляем данные в FSMContext
+    user_data = (await state.get_data()).get("user_data", {})
+    user_data["car_model"] = event.text
     await state.update_data(user_data=user_data)
-
-    # Дополнительная логика обработки ответа, если необходимо
-    # Например, задать третий вопрос или выполнить другие действия
-
-    # Задаем третий вопрос
     await event.answer("Отлично! Какой год выпуска у автомобиля?:")
-
-    # Устанавливаем состояние пользователя в STATE_CAR_YEAR
     await state.set_state(STATE_CAR_YEAR)
 
 
 @dp.message_handler(state=STATE_CAR_YEAR)
 async def get_car_year(event: types.Message, state: FSMContext):
-    user_id = event.from_user.id
-    car_year = event.text
-
-    # Получаем текущие данные из FSMContext
-    data = await state.get_data()
-    user_data = data.get("user_data", {})
-
-    # Добавляем новые данные
-    user_data["car_year"] = car_year
-
-    # Обновляем данные в FSMContext
+    user_data = (await state.get_data()).get("user_data", {})
+    user_data["car_year"] = event.text
     await state.update_data(user_data=user_data)
-
-    # Дополнительная логика обработки ответа, если необходимо
-    # Например, завершить сбор данных или выполнить другие действия
-
-    # Выводим завершающее сообщение
     await event.answer("Отлично! Какой тип кузова у автомобиля?")
 
-    # Сбрасываем состояние пользователя
-    await state.finish()
+    # Вместо state.finish() используем state.reset_state()
+    await state.reset_state(with_data=False)
 
-    # Получаем окончательные данные из FSMContext
     final_data = await state.get_data()
     user_data = final_data.get("user_data", {})
+    print("Final Data:", final_data)
+    print("User Data:", user_data)
 
-    print(final_data)
 
 if __name__ == '__main__':
     from aiogram import executor
-
-    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
