679ebef59fa9fb580259d5cc9b31f29c4f296669
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2024-01-16 09:53:54 +0300

    v.5.9

--- oop test1.py
+++ oop test1.py
@@ -6,17 +6,34 @@ from aiogram.dispatcher.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import uuid
 import asyncio
-from dicts import dict_car_brands_and_models
+from dicts import *
 
-class Car_Post:
-    def __init__(self, api_token):
-        self.api_token = api_token
-        self.bot = Bot(token=api_token)
-        self.dp = Dispatcher(self.bot, storage=MemoryStorage())
+STATE_CAR_BRAND = 'state_car_brand'
+STATE_CAR_MODEL = 'state_car_model'
+STATE_CAR_YEAR = 'state_car_year'
+STATE_CAR_BODY_TYPE = 'state_car_body_type'
+STATE_CAR_ENGINE_TYPE = 'state_car_engine_type'
+STATE_CAR_ENGINE_VOLUME = 'state_car_engine_volume'
+STATE_CAR_POWER = 'state_car_power'
+STATE_CAR_TRANSMISSION_TYPE = 'state_car_transmission_type'
+STATE_CAR_COLOR = 'state_car_color'
+STATE_CAR_MILEAGE = 'state_car_mileage'
+STATE_CAR_DOCUMENT_STATUS = 'state_car_document_status'
+STATE_CAR_OWNERS = 'state_car_owners'
+STATE_CAR_CUSTOMS_CLEARED = 'state_car_customs_cleared'
+STATE_CAR_PHOTO = 'state_car_photo'
+STATE_CAR_DESCRIPTION = 'state_car_description'
+STATE_CAR_PRICE = 'state_car_price'
+STATE_CAR_LOCATION = 'state_car_location'
+STATE_SELLER_NAME = 'state_seller_name'
+STATE_SELLER_PHONE = 'state_seller_phone'
+STATE_SEND = 'state_send'
+
+class CarBotHandler:
+    def __init__(self, ):
         self.lock = asyncio.Lock()
         self.buffered_photos = []
 
-    @staticmethod
     async def start(self, event, state):
         user_id = event.from_user.id
         user_data = await state.get_data() or {}
@@ -31,8 +48,10 @@ class Car_Post:
 
         await event.answer("Выберите бренд автомобиля:", reply_markup=keyboard)
         await state.set_state('state_car_brand')
+        # Другие методы...
 
-    async def process_brand_callback(query: types.CallbackQuery, state: FSMContext):
+
+    async def process_brand_callback(self, query, state):
         user_data = (await state.get_data()).get("user_data", {})
         selected_brand = query.data.split('_')[1]
 
@@ -54,18 +73,73 @@ class Car_Post:
 
         await query.answer()
 
+    async def process_model_callback(self, query, state):
+        user_data = (await state.get_data()).get("user_data", {})
+        selected_model = query.data.split('_')[1]
+        user_data["car_model"] = selected_model
+        await state.update_data(user_data=user_data)
+        await query.message.answer("Отлично! Какой год выпуска у автомобиля?")
+        await state.set_state('state_car_year')
+        await query.answer()
+
+    async def get_car_year(self, event, state):
+        user_data = (await state.get_data()).get("user_data", {})
+        user_data["car_year"] = event.text
+        await state.update_data(user_data=user_data)
+        await event.answer("Отлично! Какой тип кузова у автомобиля?")
+        await state.set_state(STATE_CAR_BODY_TYPE)
+
+    async def get_car_body_type(self, event, state):
+        user_data = (await state.get_data()).get("user_data", {})
+        user_data["car_body_type"] = event.text
+        await state.update_data(user_data=user_data)
+        await event.answer("Отлично! Какой тип двигателя у автомобиля?")
+        await state.set_state(STATE_CAR_ENGINE_TYPE)
+
+    async def get_car_engine_type(self, event, state):
+        user_data = (await state.get_data()).get("user_data", {})
+        user_data["car_engine_type"] = event.text
+        await state.update_data(user_data=user_data)
+        await event.answer("Хорошо! Какой объем двигателя у автомобиля?")
+        await state.set_state(STATE_CAR_ENGINE_VOLUME)
+
+
+
+
 # Использование класса
-API_TOKEN = "6803723279:AAGEujzpCZq3nMCidAt0MsZjBEMKkQUDw9M"
-car_post = Car_Post(API_TOKEN, dp=car_post.dp)
+api_token = "6803723279:AAGEujzpCZq3nMCidAt0MsZjBEMKkQUDw9M"
+car_bot = CarBotHandler()
+bot = Bot(token=api_token)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-@car_post.dp.message_handler(Command("start"))
+@dp.message_handler(Command("start"))
 async def cmd_start(event: types.Message, state: FSMContext):
-    await car_post.start(event, state)
+    await car_bot.start(event, state)
 
-@car_post.dp.callback_query_handler(lambda c: c.data.startswith('brand_'), state='state_car_brand')
+@dp.callback_query_handler(lambda c: c.data.startswith('brand_'), state='state_car_brand')
 async def process_brand_callback(query: types.CallbackQuery, state: FSMContext):
-    await car_post.process_brand_callback(query, state)
+    await car_bot.process_brand_callback(query, state)
 
+@dp.callback_query_handler(lambda c: c.data.startswith('model_'), state='state_car_model')
+async def process_model_callback(query: types.CallbackQuery, state: FSMContext):
+    await car_bot.process_model_callback(query, state)
+
+@dp.message_handler(state=STATE_CAR_YEAR)
+async def get_car_year(event: types.Message, state: FSMContext):
+    await car_bot.get_car_year(event, state)
+
+@dp.message_handler(state=STATE_CAR_BODY_TYPE)
+async def get_car_body_type(event: types.Message, state: FSMContext):
+    await car_bot.get_car_body_type(event, state)
+
+@dp.message_handler(state=STATE_CAR_ENGINE_TYPE)
+async def get_car_engine_type(event: types.Message, state: FSMContext):
+    await car_bot.get_car_engine_type(event, state)
+
+
+
+
+# старт бота
 if __name__ == '__main__':
     from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
