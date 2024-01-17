da7e99863b2887fd6e9e7648b4ff6a59b89898fe
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2024-01-17 09:51:40 +0300

    v.7.0

--- use not/oop test1.py
+++ use not/oop test1.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InputMediaPhoto
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
