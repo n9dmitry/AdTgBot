d96e2d3c11fd75f33ffb006aceff5dfff336df3d
EnotikProger <123252340+EnotikProger@users.noreply.github.com>
2024-01-18 15:36:49 +0300

    v.7.3

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ CHANNEL_ID = '@autoxyibot1'
 # API_TOKEN = '6087732169:AAHABX0K5LHguc-ymnd0Um8UOK8oucvX_gY'
 # API_TOKEN2 = '6803723279:AAGEujzpCZq3nMCidAt0MsZjBEMKkQUDw9M'
 # API_fttlolbot = '6986960778:AAGzuNdkvAfgrr5Gc2oVHfEwrWYY7NvRqJE'
+# API_TOKEN4 = '6663634927:AAFvwAxHoSh50vPwE2lAAQidxfxVuvQE7QA'
 
 # Каналы
 # @autoxyibot1
