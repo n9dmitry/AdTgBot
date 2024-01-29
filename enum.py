842854f589e7787907cc59e6a29e34678d74ac01
dev <nikitindmitry7@gmail.com>
2024-01-29 16:12:30 +0300

    v.8.3.2. - enum

---
+++ enum.py
@@ -0,0 +1,25 @@
+class ImageDirectory:
+    car_brand = "/path/to/car_brand.jpg"
+    car_model = "/path/to/car_model.jpg"
+    car_year = "/path/to/car_year.jpg"
+    car_mileage = "/path/to/car_mileage.jpg"
+    car_transmission_type = "/path/to/car_transmission_type.jpg"
+    car_body_type = "/path/to/car_body_type.jpg"
+    car_engine_type = "/path/to/car_engine_type.jpg"
+    car_engine_volume = "/path/to/car_engine_volume.jpg"
+    car_power = "/path/to/car_power.jpg"
+    car_color = "/path/to/car_color.jpg"
+    car_document_status = "/path/to/car_document_status.jpg"
+    car_owners = "/path/to/car_owners.jpg"
+    car_customs_cleared = "/path/to/car_customs_cleared.jpg"
+    car_condition = "/path/to/car_condition.jpg"
+    car_description = "/path/to/car_description.jpg"
+    car_price = "/path/to/car_price.jpg"
+    car_location = "/path/to/car_location_map.jpg"
+    seller_name = "/path/to/seller_name.jpg"
+    seller_phone = "/path/to/seller_phone.jpg"
+
+
+# Пример использования
+image_to_load = ImageDirectory.IMG_1
+print(image_to_load)  # Вывод: /img/1.jpg
