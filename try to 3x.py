29af0f7aabd5a1b5d37b06351aff409d37e48f76
dev <nikitindmitry7@gmail.com>
2024-02-15 15:53:35 +0300

    v.8.5.3. -  try aiogram 3x

--- try to 3x.py
+++ try to 3x.py
@@ -1,11 +1,11 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F, Router, html, types
-from aiogram.types import InputMediaPhoto
+from aiogram import Bot, Dispatcher, Router, F, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InputMediaPhoto
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.filters import Command, CommandStart
 import random
 import datetime
 import uuid
@@ -15,6 +15,19 @@ from states import *
 from validation import *
 import json
 from enumlist import *
+
+async def main():
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+router = Router(name=__name__)
+buffered_photos = []
+lock = asyncio.Lock()
+storage=MemoryStorage()
+session = AiohttpSession()
+bot_settings = {"session": session, "parse_mode": ParseMode.HTML}
+bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
 
 # Загрузка JSON в начале скрипта
 with open('dicts.json', 'r', encoding='utf-8') as file:
@@ -37,25 +50,26 @@ dict_edit_buttons = dicts.get("dict_edit_buttons", {})
 
 # Создание клавиатуры
 def create_keyboard(button_texts, resize_keyboard=True):
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=resize_keyboard, row_width=2)
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard, row_width=2)
     buttons = [KeyboardButton(text=text) for text in button_texts]
     keyboard.add(*buttons)
     return keyboard
 
 
-
 class CarBotHandler:
     def __init__(self):
         self.lock = asyncio.Lock()
+        self.car_bot = CarBotHandler()
 
 # Команды
-    async def restart(self, event, state):
+    @router.message(F.text == "Перезагрузить бота")
+    async def restart(self, event: types.Message, state: FSMContext):
         await state.finish()
         await event.answer("Бот перезапущен.")
         await self.start(event, state)
 
-    async def support(self, event, state):
+    @router.message(Command("support"))
+    async def support(self, event: types.Message, state: FSMContext):
         await state.finish()
         self.secret_number = str(random.randint(100, 999))
 
@@ -63,7 +77,8 @@ class CarBotHandler:
                              f"Но перед этим введите проверку. Докажите что вы не робот. Напишите число {self.secret_number}:")
         await state.set_state(User.STATE_SUPPORT_VALIDATION)
 
-    async def support_validation(self, event, state):
+    @router.message(User.STATE_SUPPORT_VALIDATION)
+    async def support_validation(self, event: types.Message, state: FSMContext):
         if event.text.isdigit() and event.text == self.secret_number:
             await event.reply(f"Проверка пройдена успешно!")
             await asyncio.sleep(1)
@@ -72,9 +87,10 @@ class CarBotHandler:
         else:
             await event.answer(f"Попробуйте ещё раз!")
             await asyncio.sleep(1)
-            await cmd_support(event, state)
+            await self.car_bot.support(event, state)
 
-    async def support_message(self, event: types.Message, state):
+    @router.message(User.STATE_SUPPORT_MESSAGE)
+    async def support_message(self, event: types.Message, state: FSMContext):
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message_to_write = f"""
         Дата: {current_time}
@@ -91,31 +107,30 @@ class CarBotHandler:
         keyboard = create_keyboard(['Перезагрузить бота'])
         await event.reply("Спасибо за ваше сообщение! Мы рассмотрим вашу проблему!", reply_markup=keyboard)
         await state.set_state(User.STATE_SUPPORT_END)
-    async def support_end(self, event, state):
-        if event.text == 'Перезагрузить бота':
-            await cmd_restart(event, state)
-        await state.finish()
 
+    @router.message(User.STATE_SUPPORT_END)
+    async def support_end(self, event: types.Message, state: FSMContext):
+        await self.car_bot.restart(event, state)
 
 
 # Начало работы бота
 
-    async def start(self, event, state):
+    @router.message(CommandStart())
+    async def start(message: types.Message, state: FSMContext):
         image_hello_path = ImageDirectory.say_hi
         with open(image_hello_path, "rb") as image_hello:
-            await event.answer_photo(image_hello,
-                                     caption=f"Привет, {event.from_user.first_name}! Давай продадим твоё авто! Начнём же сбор данных!")
+            input_file = types.InputFile(image_hello)
+            await bot.send_photo(chat_id=message.from_user.id, photo=input_file,
+                                     caption=f"Привет, {message.from_user.first_name}! Давай продадим твоё авто! Начнём же сбор данных!")
         await asyncio.sleep(0)
         keyboard = create_keyboard(list(dict_car_brands_and_models.keys()))
         image_path = ImageDirectory.car_brand
         with open(image_path, "rb") as image:
-            await event.answer_photo(image, caption="Выберите бренд автомобиля:", reply_markup=keyboard)
+            await bot.send_photo(image, caption="Выберите бренд автомобиля:", reply_markup=keyboard)
         await state.set_state(User.STATE_CAR_BRAND)
 
-
-
-
-    async def get_car_brand(self, event, state):
+    @router.message(User.STATE_CAR_BRAND)
+    async def get_car_brand(self, event: types.Message, state: FSMContext):
         user_data = (await state.get_data()).get("user_data", {})
         selected_brand = event.text
         valid_brands = dict_car_brands_and_models
@@ -133,8 +148,8 @@ class CarBotHandler:
             await bot.send_message(event.from_user.id, "Пожалуйста, выберите бренд из предложенных вариантов или напишите нам если вашего бренда нет", reply_markup=keyboard)
             await state.set_state(User.STATE_CAR_PHOTO)
 
-
-    async def handle_photos(self, event, state):
+    @router.message(User.STATE_CAR_PHOTO, F.photo)
+    async def handle_photos(self, event: types.Message, state: FSMContext):
         user_data = await state.get_data('user_data')
         photo_id = event.photo[-1].file_id
 
@@ -223,7 +238,8 @@ class CarBotHandler:
         sheet.append(row_data)
         workbook.save(file_path)
 
-    async def preview_advertisement(self, event):
+    @router.message(F.text == "Следущий шаг")
+    async def preview_advertisement(self, event: types.Message):
         await bot.send_media_group(chat_id=event.chat.id, media=buffered_photos, disable_notification=True)
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(
             KeyboardButton("Отправить в канал"),
@@ -231,7 +247,8 @@ class CarBotHandler:
         )
         await event.reply("Так будет выглядеть ваше объявление. Вы можете либо разместить либо отменить и заполнить заново.", reply_markup=keyboard)
 
-    async def send_advertisement(self, event):
+    @router.message(F.text == "Отправить в канал")
+    async def send_advertisement(self, event: types.Message):
         async with lock:
             user_id = event.from_user.id
             await self.add_data_to_excel(event)
@@ -241,8 +258,8 @@ class CarBotHandler:
 
             buffered_photos.clear()
 
-
-    async def fill_again(self, event, state):
+    @router.message(F.text == "Отменить и заполнить заново")
+    async def fill_again(self, event: types.Message, state: FSMContext):
         keyboard = create_keyboard(list(dict_car_brands_and_models.keys()))
         image_path = ImageDirectory.car_brand # Путь к вашему изображению
         with open(image_path, "rb") as image:
@@ -251,85 +268,17 @@ class CarBotHandler:
             buffered_photos.clear()
         await state.set_state(User.STATE_CAR_BRAND)
 
-    async def add_more(self, event, state):
-        await car_bot.restart(event, state)
+    @router.message(F.text == "Добавить ещё объявление")
+    async def add_more(self, event: types.Message, state: FSMContext):
+        await self.car_bot.restart(event, state)
 
-    async def promotion(self, event, state):
+    @router.message(F.text == "Ускорить продажу")
+    async def promotion(self, event: types.Message):
         keyboard = create_keyboard(['Перезагрузить бота'])
         await event.reply("Чтобы купить закреп напишите @n9dmitry", reply_markup=keyboard)
-
-
-car_bot = CarBotHandler()
-session = AiohttpSession()
-bot_settings = {"session": session, "parse_mode": ParseMode.HTML}
-bot = Bot(token=API_TOKEN, **bot_settings)
-dp = Dispatcher(bot, storage=MemoryStorage())
-router = Router(name=__name__)
-lock = asyncio.Lock()
-buffered_photos = []
-dp.include_router(router)
-
-
-@router.message(commands=['restart'], state='*')
-async def cmd_restart(event: types.Message, state: FSMContext):
-    await car_bot.restart(event, state)
-
-@router.message(lambda message: message.text == "Перезагрузить бота", state='*')
-async def cmd_restart(event: types.Message, state: FSMContext):
-    await car_bot.restart(event, state)
-
-@router.message(commands=["start"])
-async def cmd_start(event: types.Message, state: FSMContext):
-    await car_bot.start(event, state)
-
-#support
-@router.message(commands=['support'], state='*')
-async def cmd_support(event: types.Message, state: FSMContext):
-    await car_bot.support(event, state)
-
-@router.message(state=User.STATE_SUPPORT_VALIDATION)
-async def support_validation(event: types.Message, state: FSMContext):
-    await car_bot.support_validation(event, state)
-
-@router.message(state=User.STATE_SUPPORT_MESSAGE)
-async def support_message(event: types.Message, state: FSMContext):
-    await car_bot.support_message(event, state)
-
-@router.message(state=User.STATE_SUPPORT_END)
-async def support_end(event: types.Message, state: FSMContext):
-    await car_bot.restart(event, state)
 # end support
-
-@router.message(state=User.STATE_CAR_BRAND)
-async def process_brand_selection(event: types.Message, state: FSMContext):
-    await car_bot.get_car_brand(event, state)
-
-@router.message(state=User.STATE_CAR_PHOTO, content_types=['photo'])
-async def handle_photos(event: types.Message, state: FSMContext):
-    await car_bot.handle_photos(event, state)
-
-@router.message(lambda message: message.text == "Следущий шаг")
-async def preview_advertisement(event: types.Message):
-    await car_bot.preview_advertisement(event)
-
-
-@router.message(lambda message: message.text == "Отправить в канал")
-async def send_advertisement(event: types.Message, state: FSMContext):
-    await car_bot.send_advertisement(event)
-
-@router.message(lambda message: message.text == "Отменить и заполнить заново")
-async def fill_again(event: types.Message, state: FSMContext):
-    await car_bot.fill_again(event, state)
-
-@router.message(lambda message: message.text == "Добавить ещё объявление")
-async def add_more(event: types.Message, state: FSMContext):
-    await car_bot.add_more(event, state)
-
-@router.message(lambda message: message.text == "Ускорить продажу")
-async def promotion(event: types.Message, state: FSMContext):
-    await car_bot.promotion(event, state)
 
 
 # старт бота
 if __name__ == '__main__':
-    asyncio.run()
+    asyncio.run(main())
